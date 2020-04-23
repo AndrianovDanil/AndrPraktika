1ecf90562cf03459b5e924a9b387cfb1526f2936
lucifer_seidens@mail.ru <lucifer_seidens@mail.ru>
2020-04-23 17:38:43 +0300

    Logger

--- AndrPraktika000/App.xaml.cs
+++ AndrPraktika000/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ModuleApplicationProto;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace AndrPraktika000
     /// </summary>
     public partial class App : Application
     {
+        ILogger logger = new Logger();
     }
 }
