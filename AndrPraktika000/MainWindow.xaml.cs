abce264ee4650b2acbe4eeba8766c6d497bc7c3b
lucifer_seidens@mail.ru <lucifer_seidens@mail.ru>
2020-04-30 19:51:40 +0300

    Добавление текст. файлов и открытие их

--- AndrPraktika000/MainWindow.xaml.cs
+++ AndrPraktika000/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +29,52 @@ namespace AndrPraktika000
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Hello, world!", "Сообщение");
+
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            Settings taskWindow = new Settings();
+            taskWindow.Show();
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("Группа 9ИСП-391к-17");
+            sb.AppendLine("Васильев А.");
+            sb.AppendLine("Андрианов Д.");
+            sb.AppendLine("Наша ссылка на GitHub:");
+            sb.AppendLine("https://github.com/AndrianovDanil/AndrVasVPraktika/tree/master2/AndrPraktika000");
+            MessageBox.Show(sb.ToString());
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            WrapPanel wrapPanel = new WrapPanel();
+            MessageBox.Show("Поле очищено");
+        }
+        private void newbutton(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                    Andr.Items.Add(System.IO.Path.GetFileName(filename));
+            }
+        }
+        private void newbutton1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                textbox.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+        private void textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
